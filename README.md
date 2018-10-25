76a046eac472cd2845fb6bae0a8f639b4463a736
Luke Berry <contact@luke-berry.co.uk>
2018-10-25 16:30:01 +0100

    init commit

---
+++ README.md
@@ -0,0 +1,4 @@
+* Listen for a registered action
+* Check the pre-requisites
+* Prevent the action, or allow it
+* What happens after it's been created?
---
+++ src/Providers/RulesSerivceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Automise\Rules\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class RulesServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+    }
+
+    public function register()
+    {
+    }
+}
